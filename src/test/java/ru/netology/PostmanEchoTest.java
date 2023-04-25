f4968749b4bb6c8afb0528a7163fa488d6c66acd
Ivan Korolev <ikorolev13@gmail.com>
2023-04-25 19:14:52 +0300

    IKorolev/main/ failed test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -1,11 +1,11 @@
 package ru.netology;
 
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 public class PostmanEchoTest {
     @Test
@@ -14,14 +14,13 @@ public class PostmanEchoTest {
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Vasya_55") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("") // отправляемые данные (заголовки и query можно выставлять аналогично)
                 // Выполняемые действия
                 .when()
                 .post("/post")
                 // Проверки
                 .then().log().all()
                 .statusCode(200)
-                .body("data", equalTo("Vasya_55"))
-                .body("headers.content-length", equalTo("8"));
+                .body("headers.content-length", greaterThanOrEqualTo("2"));
     }
 }
